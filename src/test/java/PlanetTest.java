c7739ebb937a0f539cc9a8f3a13f600cdd39e227
Alastair McGilloway <alastairmcgillowaY@hotmail.com>
2020-01-27 14:57:35 +0000

    Initial Commit

---
+++ src/test/java/PlanetTest.java
@@ -0,0 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlanetTest {
+
+    private Planet mars;
+
+    @Before
+    public void before(){
+        mars = new Planet("Mars", 908973);
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Mars", mars.getName());
+    }
+
+    @Test
+    public void hasSize() {
+        assertEquals(908973, mars.getSize());
+    }
+
+    @Test
+    public void explode() {
+        assertEquals("Boom! Mars has exploded", mars.explode());
+    }
+}
